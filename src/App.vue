a7c287c4a018ee0af3749815d3cd78d80e0bfa32
taiphan2288 <67281521+taiphan2288@users.noreply.github.com>
2022-05-22 16:11:33 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="h-100">
+    <div class="container-fluid h-100">
+      <div class="row h-100">
+        <div
+          class="col-md-3 vue-bg h-100 d-flex justify-content-center align-items-center"
+        >
+          <img alt="Vue logo" src="./assets/logo.png" width="100" />
+        </div>
+        <div
+          class="col-md-9 h-100 d-flex justify-content-center align-items-center"
+        >
+          <div class="col-md-8 rounded px-5 py-4 shadow bg-white text-left">
+            <SignupForm />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import SignupForm from "./components/SignupForm.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    SignupForm,
+  },
+};
 </script>
 
 <style>
@@ -21,8 +34,10 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.vue-bg {
+  background: #bce5c0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import Vuelidate from "vuelidate";
+import axios from "axios";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(Vuelidate);
+Vue.prototype.$http = axios;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
